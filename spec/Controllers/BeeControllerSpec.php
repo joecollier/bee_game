3c507c3b28f23d20adcc6cc77029759ae8c44c96
Joseph Collier <you@example.com>
2016-12-12 22:31:00 -0500

    Additional kahlan tests and functionality

--- spec/Controllers/BeeControllerSpec.php
+++ spec/Controllers/BeeControllerSpec.php
@@ -5,7 +5,7 @@
 
     describe(BeeController::class, function () {
         describe('::deductHitPoints', function () {
-            it('deducts hitpoints of a queen with 60 current hitpoints', function () {
+            it('deducts hitpoints of a queen with 200 current hitpoints', function () {
                 $this->bee = new Bee('queen');
                 $this->bee_controller = new BeeController($this->bee);
                 $this->bee_controller->hitBee();
