561b893f585dfee1ffb9f22e9ee384c503adc6a9
Joseph Collier <you@example.com>
2017-03-08 21:21:47 -0500

    [ci_and_githooks] Fixing View (accidentally removed template)

---
+++ src/Views/index.html.php
@@ -0,0 +1,55 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <link rel='stylesheet' type='text/css' href='assets/main.css' />
+    </head>
+    <body>
+        <div id="container">
+            <center>
+                <div id="title">Welcome to the Bee Game!</div>
+                <div id='bee-counts' style='height:220px;'>
+                    <?php
+                        if ($counts['queen'] > 0) {
+                            echo "<div style='height:100%;'><div>";
+
+                            $last_type = '';
+
+                            foreach ($game_data as $bee) {
+                                $div_clear = '';
+                                $bold = '';
+
+                                $type = $bee->type;
+
+                                if ($type !== $last_type) {
+                                    echo "<br>";
+                                }
+
+                                if ($last_hit == $type) {
+                                    $bold = '-bold';
+                                }
+
+                                $count = $counts[$type];
+
+                                if ($type !== $last_type) {
+                                    echo "<div class='bee-status-type{$bold}'>Remaining {$type}s:</div>";
+                                }
+                                echo "<div class='bee-cell'>" . $bee->hitpoints . "</div>";
+
+                                $last_type = $type;
+                            }
+
+                            echo "</div><div id='hit-button'><div>{$hit_count} hits</div>";
+                        } else {
+                            echo "<div style='height:100%;'>Game completed in {$hit_count} hits</div>";
+                        }
+                    ?>
+                    <div>
+                        <form method="post" action="index.php">
+                            <input id="button" type="submit" value="hit" name="submit">
+                        </form>
+                    </div>
+                </div>
+            </center>
+        </div>
+    </body>
+</html>
